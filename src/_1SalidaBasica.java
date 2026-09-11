a706d237096afe5e811546798e81975a2547d7dc
Juan Antonio Cuello <jacuela@gmail.com>
2026-09-11 20:08:17 +0200

    1 salida basica

---
+++ src/_1SalidaBasica.java
@@ -0,0 +1,20 @@
+public class _1SalidaBasica {
+    public static void main(String[] args) {
+        System.out.print("Imprimo sin saltar la linea.");
+        System.out.println("Continuo en la linea anterior");
+        System.out.println("Esto ya va en otra linea");
+        System.out.println();//meto un salto de linea
+        System.out.println("\\u001B[31mTexto en rojo\\u001B[0m");
+        System.out.println("Cruz en UNICODE:\u2020");
+
+        //Esto es un comentario de 1 línea
+
+        /* Esto es un comentario de
+           varias líneas.
+        */
+
+        //Esto es un comentario de
+        //varias líneas.
+    }
+
+}
